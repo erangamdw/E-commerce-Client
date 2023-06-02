7dea359e935062e4778233b5f49406585229ad23
erangamdw <erangamdw@gmail.com>
2023-06-02 17:27:37 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+
+const router =createBrowserRouter([
+  {
+    path:'',
+    element:<Home/>
+  },
+  {
+    path:'/products/:id',
+    element:<Products/>
+  },
+  {
+    path:'/product/:id',
+    element:<Product/>
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
